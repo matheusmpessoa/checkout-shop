6e2e3403ee9a690760e58de13bd37326700d6b55
Matheus Pessoa <matheusmpessoa17@gmail.com>
2020-04-25 13:02:13 -0300

    Create form and fix layout

--- checkout-shop/src/app/pages/checkout/checkout.component.ts
+++ checkout-shop/src/app/pages/checkout/checkout.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface Installments {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +12,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
+  carrinhoForm: FormGroup;
+  isLinear = true;
 
-  constructor() { }
+  numberOfInstallment: Installments[] = [
+    { value: '12', viewValue: '12x R$1.000,00 sem juros' },
+    { value: '10', viewValue: '10x R$1.200,00 sem juros' },
+    { value: '6', viewValue: '6x R$2.000,00 sem juros' },
+    { value: '2', viewValue: '2x R$6.000,00 sem juros' },
+    { value: '1', viewValue: 'A vista' }
+  ];
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.carrinhoForm = this.fb.group({
+      cardNumber: [''],
+      fullName: [''],
+      expirationMonth: [''],
+      securityCode: ['']
+    });
   }
 
+  onSubmit() {
+    console.log(this.carrinhoForm.value.username);
+  }
 }
--- checkout-shop/src/app/services/db.ts
+++ checkout-shop/src/app/services/db.ts
@@ -1,82 +1,83 @@
-const clients = [{
-  id: 1,
-  client: {
-    fullName: 'Bogey Muston',
-    firstName: 'Bogey',
-    lastName: 'Muston',
-    cpf: 11,
-    gender: 'Male',
-    dateOfBirth: '2/12/1997',
-    email: 'bmuston0@fakemail.com',
-    phone: '+86 (817) 545-0780',
-    address: '4920 Quincy Circle'
-  },
-  creditCard: {
-    cardNumber: '36525075919880',
-    expirationMonth: '02',
-    expirationYear: '2022',
-    securityCode: '849',
-    brand: 'diners-club-international'
+const clients = [
+  {
+    id: 1,
+    client: {
+      fullName: 'Bogey Muston',
+      firstName: 'Bogey',
+      lastName: 'Muston',
+      cpf: '349.857.826-11',
+      gender: 'Male',
+      dateOfBirth: '2/12/1997',
+      email: 'bmuston0@fakemail.com',
+      phone: '+86 (817) 545-0780',
+      address: '4920 Quincy Circle'
+    },
+    creditCard: {
+      cardNumber: '3652 5075 9198 7880',
+      expirationMonth: '02',
+      expirationYear: '2022',
+      securityCode: '849',
+      brand: 'diners-club-international'
+    }
+  }, {
+    id: 2,
+    client: {
+      fullName: 'Jarrett Battman',
+      firstName: 'Jarrett',
+      lastName: 'Battman',
+      cpf: '827.384.847-72',
+      gender: 'Male',
+      dateOfBirth: '12/17/1996',
+      email: 'jbattman1@fakemail.com',
+      phone: '+86 (313) 268-9788',
+      address: '62013 Elka Park'
+    },
+    creditCard: {
+      cardNumber: '4903 6538 90208 3501 8751',
+      expirationMonth: '08',
+      expirationYear: '2028',
+      securityCode: '934',
+      brand: 'visa'
+    }
+  }, {
+    id: 3,
+    client: {
+      fullName: 'Matt Grinvalds',
+      firstName: 'Matt',
+      lastName: 'Grinvalds',
+      cpf: '359.839.178-90',
+      gender: 'Male',
+      dateOfBirth: '12/11/1993',
+      email: 'mgrinvalds2@fakemail.com',
+      phone: '+7 (397) 750-9192',
+      address: '3716 Heffernan Park'
+    },
+    creditCard: {
+      cardNumber: '6759 1054 3651 4346 6646',
+      expirationMonth: '10',
+      expirationYear: '2024',
+      securityCode: '910',
+      brand: 'maestro'
+    }
+  }, {
+    id: 4,
+    client: {
+      fullName: 'Parrnell Tetford',
+      firstName: 'Parrnell',
+      lastName: 'Tetford',
+      cpf: '449.891.850-70',
+      gender: 'Male',
+      dateOfBirth: '9/12/1990',
+      email: 'ptetford3@fakemail.com',
+      phone: '+86 (948) 977-7793',
+      address: '361 Browning Pass'
+    },
+    creditCard: {
+      cardNumber: '3549 1992 9409 4588',
+      expirationMonth: '04',
+      expirationYear: '2023',
+      securityCode: '497',
+      brand: 'jcb',
+    }
   }
-}, {
-  id: 2,
-  client: {
-    fullName: 'Jarrett Battman',
-    firstName: 'Jarrett',
-    lastName: 'Battman',
-    cpf: 11,
-    gender: 'Male',
-    dateOfBirth: '12/17/1996',
-    email: 'jbattman1@fakemail.com',
-    phone: '+86 (313) 268-9788',
-    address: '62013 Elka Park'
-  },
-  creditCard: {
-    cardNumber: '4903653890208350111',
-    expirationMonth: '08',
-    expirationYear: '2028',
-    securityCode: '934',
-    brand: 'visa'
-  }
-}, {
-  id: 3,
-  client: {
-    fullName: 'Matt Grinvalds',
-    firstName: 'Matt',
-    lastName: 'Grinvalds',
-    cpf: 11,
-    gender: 'Male',
-    dateOfBirth: '12/11/1993',
-    email: 'mgrinvalds2@fakemail.com',
-    phone: '+7 (397) 750-9192',
-    address: '3716 Heffernan Park'
-  },
-  creditCard: {
-    cardNumber: '675910543651434646',
-    expirationMonth: '10',
-    expirationYear: '2024',
-    securityCode: '910',
-    brand: 'maestro'
-  }
-}, {
-  id: 4,
-  client: {
-    fullName: 'Parrnell Tetford',
-    firstName: 'Parrnell',
-    lastName: 'Tetford',
-    cpf: 11,
-    gender: 'Male',
-    dateOfBirth: '9/12/1990',
-    email: 'ptetford3@fakemail.com',
-    phone: '+86 (948) 977-7793',
-    address: '361 Browning Pass'
-  },
-  creditCard: {
-    cardNumber: '3549199294094588',
-    expirationMonth: '04',
-    expirationYear: '2023',
-    securityCode: '497',
-    brand: 'jcb',
-  }
-}
 ];
